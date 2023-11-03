931292a3d09248d6e2165cc47c4a335af011c827
Pedro Castro <pedro.tiago.castro.ist@gmail.com>
2023-11-03 18:19:57 +0000

    Index and About routes

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const app = express()
 
 const port = 5000
 
+// Index Route
+app.get("/", (req, res) => {
+    res.send("INDEX")
+})
+
+// About Route
+app.get("/", (req, res) => {
+    res.send("ABOUT")
+})
+
 app.listen(port, () => {
     console.log(`Server started on port ${port}`)
 })
